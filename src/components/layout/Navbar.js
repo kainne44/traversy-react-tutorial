996ec1f334a940168b20754b3dc2fece9dcf47d2
kainne44 <mrmaclaine44@gmail.com>
2020-04-21 19:16:06 -0400

    Got profiles from api, implemented search method, validated search input

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,21 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github",
-  };
-  render() {
-    return (
-      <div>
-        <nav className='navbar bg-primary'>
-          <h1>
-            <i className={this.props.icon} /> {this.props.title}
-          </h1>
-        </nav>
-      </div>
-    );
-  }
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <div>
+      <nav className='navbar bg-primary'>
+        <h1>
+          <i className={icon} /> {title}
+        </h1>
+      </nav>
+    </div>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
